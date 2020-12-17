61bf52cd5d5838ba10cb80476253dc524d2c67e6
wuchao <n2s_LZ39>
2020-12-17 20:48:18 +0800

    增加api示例

---
+++ src/test/java/com/junit5/service/BmpTest.java
@@ -0,0 +1,37 @@
+package com.junit5.service;
+
+import io.netty.handler.codec.http.HttpResponse;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.filters.ResponseFilter;
+import net.lightbody.bmp.util.HttpMessageContents;
+import net.lightbody.bmp.util.HttpMessageInfo;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class BmpTest {
+    @Test
+    public void bmpProxyTest(){
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+        int port = proxy.getPort(); // get the JVM-assigned port
+        // Selenium or HTTP client configuration goes here
+
+        proxy.addResponseFilter(new ResponseFilter() {
+            @Override
+            public void filterResponse(HttpResponse response, HttpMessageContents contents, HttpMessageInfo messageInfo) {
+                if (messageInfo.getOriginalUrl().contains(".json")) {
+                    //获取数据如是json可转为hashmap，然后使用递归改成新的hashmap，然后再转为json塞回文本里即可替换，以下是简单实现
+                    String contentsNew = contents.getTextContents().replaceAll("\"[^\"]*\"","null");
+                    contents.setTextContents(contentsNew);
+                }
+            }
+        });
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/junit5/service/MockDemo.java
@@ -0,0 +1,74 @@
+package com.junit5.service;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+public class MockDemo {
+    private static WireMockServer wireMockServer;
+    @BeforeAll
+    public static void beforeAll(){
+        wireMockServer = new WireMockServer(wireMockConfig().port(8089)); //No-args constructor will start on port 8080, no HTTPS
+        wireMockServer.start();
+        configureFor("localhost",8089);
+    }
+
+    @Test
+    public void stubMock() {
+        stubFor(get(urlEqualTo("/my/resource"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>hogwarts</response>")));
+        try {
+            Thread.sleep(500000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void easyMock() {
+        stubFor(get(urlEqualTo("/my/resource"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>hogwarts</response>")));
+        try {
+            Thread.sleep(10000);
+            reset();
+            stubFor(get(urlEqualTo("/my/resource"))
+                    .withHeader("Accept", equalTo("text/xml"))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "text/xml")
+                            .withBody("<response>名企定向4期</response>")));
+            Thread.sleep(500000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void proxyMock(){
+        stubFor(get(urlMatching(".*")).atPriority(10)
+                .willReturn(aResponse().proxiedFrom("https://ceshiren.com")));
+
+        try {
+            stubFor(get(urlEqualTo("/categories_and_latest")).atPriority(10)
+                    .willReturn(aResponse().withBody(Files.readAllBytes(Paths.get(MockDemo.class.getResource("/mock.json").getPath())))));
+            Thread.sleep(50000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
